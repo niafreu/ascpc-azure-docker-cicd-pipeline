b09104c3abcc43b34addf2f04da96d7b41e5bce7
niafreu <niafreu@users.noreply.github.com>
2025-05-11 13:49:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Welcome to your ASCPC web app!')
 })
 
+app.get('/u89jg3own984hoenrdbf0eirhonbdf', (req, res) => {
+  var a = process.env
+  res.send('lala' + a)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
